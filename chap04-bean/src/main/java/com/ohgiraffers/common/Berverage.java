f4a5363a448efe4fb8cca7684a92e5f094ab3190
dansun2 <dansunmusik7@gmail.com>
2024-06-21 09:42:53 +0900

    240621

---
+++ chap04-bean/src/main/java/com/ohgiraffers/common/Berverage.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.common;
+
+public class Berverage extends Product{
+    private int capacity;
+
+    public Berverage() {
+    }
+
+    public Berverage(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Berverage(String name, int price, int capacity) {
+        super(name, price);
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " " + this.capacity;
+    }
+}
